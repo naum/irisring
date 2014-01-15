a1663b269bb463c1050dd0f626bc596e2083f64d
GNT <aznaum@gmail.com>
2014-01-14 19:20:29 -0700

    beginning spirals on a web interface

--- web/tagreport.dart
+++ web/tagreport.dart
@@ -1,5 +1,11 @@
 import 'dart:html';
 
+var tumblrBlog = querySelector('#tumblr_blog');
+var tumblrFetch = querySelector('#tumblr_fetch');
+var tumblrTagReport = querySelector('#tumblr_tagreport');
+
 main() {
-  
+  tumblrFetch.onClick.listen((e) {
+    tumblrTagReport.innerHtml = 'You entered <em>${tumblrBlog.value}</em>';
+  });
 }
