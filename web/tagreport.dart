02b8f570c3696add46a8da8e8fcaae2cfc87920c
GNT <aznaum@gmail.com>
2014-01-14 19:40:51 -0700

    another spiral for tagreport.dart

--- web/tagreport.dart
+++ web/tagreport.dart
@@ -1,11 +1,20 @@
 import 'dart:html';
+import 'dart:js';
 
 var tumblrBlog = querySelector('#tumblr_blog');
 var tumblrFetch = querySelector('#tumblr_fetch');
 var tumblrTagReport = querySelector('#tumblr_tagreport');
 
 main() {
-  tumblrFetch.onClick.listen((e) {
-    tumblrTagReport.innerHtml = 'You entered <em>${tumblrBlog.value}</em>';
-  });
+  context['processTumblrData'] = (resp) {
+    tumblrTagReport.innerHtml = resp['tumblelog']['description'];
+  };
+  tumblrFetch.onClick.listen(fetchTagData);
+}
+
+fetchTagData(e) {
+  tumblrTagReport.innerHtml = 'You entered <em>${tumblrBlog.value}</em>';
+  ScriptElement script = new Element.tag('script');
+  script.src = 'http://${tumblrBlog.value}/api/read/json/?callback=processTumblrData';
+  document.body.children.add(script);
 }
