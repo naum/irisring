d461d17f8084999830efa6dceed0bf39c94a8740
GNT <aznaum@gmail.com>
2014-01-22 11:22:58 -0700

    added frequency table output to tagreport.*

--- web/tagreport.dart
+++ web/tagreport.dart
@@ -1,16 +1,23 @@
 import 'dart:html';
 import 'dart:js';
 
+var tagFreqTable = {};
 var tumblrBlog = querySelector('#tumblr_blog');
+var tumblrConsole = querySelector('#tumblr_console');
+var tumblrInfo = querySelector('#tumblr_info');
 var tumblrFetch = querySelector('#tumblr_fetch');
 var tumblrTagReport = querySelector('#tumblr_tagreport');
+var tumblrTotalPosts = 0;
 
 main() {
   context['primeTumblrPursuit'] = (resp) {
+    tumblrTotalPosts = resp['posts-total'];
     tumblrTagReport.innerHtml = resp['tumblelog']['description'];
+    tumblrInfo.innerHtml = '${tumblrTotalPosts}';
     fetchTagData();
   };
   context['processTumblrData'] = (resp) {
+    tallyTags(resp);
     tumblrTagReport.innerHtml = generateTagReport(resp);
   };
   tumblrFetch.onClick.listen(launchTumblrFetch);
@@ -25,17 +32,34 @@ fetchTagData() {
 
 String generateTagReport(td) {
   var sb = new StringBuffer();
-  for (var p in td['posts']) {
-    if (p.hasProperty('tags')) {
-      sb.write("${p['id']} ${p['tags']}<br>");
-    }
-  }
+  sb.write('<table>');
+  tagFreqTable.forEach((tn, tt) {
+    sb.write('<tr><td class="celtex">${tn}<td class="celnum">${tt}</tr>');
+  });
+  sb.write('</table>');
   return sb.toString();
 }
 
 launchTumblrFetch(e) {
+  tagFreqTable.clear();
+  tumblrConsole.innerHtml = '';
   tumblrTagReport.innerHtml = 'Preparing launch...';
   ScriptElement script = new Element.tag('script');
   script.src = 'http://${tumblrBlog.value}/api/read/json/?callback=primeTumblrPursuit';
   document.body.children.add(script);
+}
+
+tallyTags(td) {
+  for (var p in td['posts']) {
+    tumblrConsole.innerHtml += ("${p['id']} ${p['tags']}<br>");
+    if (p.hasProperty('tags')) {
+      for (var t in p['tags']) {
+        if (tagFreqTable.containsKey(t)) {
+          tagFreqTable[t] += 1;
+        } else {
+          tagFreqTable[t] = 1;
+        }
+      }
+    }
+  }
 }
