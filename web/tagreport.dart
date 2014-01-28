a947534c2c4766824f7e083646f014235d5496a9
GN Trifanoff <aznaum@gmail.com>
2014-01-28 14:20:31 -0700

    sorted tag frequency display, table prettify

--- web/tagreport.dart
+++ web/tagreport.dart
@@ -3,7 +3,15 @@ import 'dart:html';
 import 'dart:js';
 import 'dart:math' show min;
 
+const MAX_POST_FETCH = 1000;
+
 var tagFreqTable = {};
+var tagStatTable = {
+  'POST_COUNT': 0,
+  'TAG_COUNT': 0,
+  'TAGGED': 0,
+  'UNTAGGED': 0
+};
 var tumblrBlog = querySelector('#tumblr_blog');
 var tumblrConsole = querySelector('#tumblr_console');
 var tumblrInfo = querySelector('#tumblr_info');
@@ -20,7 +28,7 @@ main() {
   };
   context['processTumblrData'] = (resp) {
     tallyTags(resp);
-    tumblrInfo.innerHtml = '';
+    tumblrInfo.innerHtml = formatTagStatInfo();
     tumblrTagReport.innerHtml = generateTagReport(resp);
   };
   tumblrFetch.onClick.listen(launchTumblrFetch);
@@ -32,9 +40,19 @@ affixExternalScriptJson(String s) {
   document.body.children.add(script);
 }
 
+int byTally(a, b) {
+  return tagFreqTable[b] - tagFreqTable[a];
+}
+
+clearStatTable() {
+  for (var k in tagStatTable.keys) {
+    tagStatTable[k] = 0;
+  }
+}
+
 fetchTagData() {
   var tb = tumblrBlog.value;
-  var endPost = min(int.parse(tumblrTotalPosts), 1000);
+  var endPost = min(int.parse(tumblrTotalPosts), MAX_POST_FETCH);
   for (var n = 0; n < endPost; n += 50) {
     var du = new Duration(seconds: (n ~/ 50) * 6);
     new Timer(du, () {
@@ -45,17 +63,29 @@ fetchTagData() {
   }
 }
 
+String formatTagStatInfo() {
+  var tp = tagStatTable['POST_COUNT'];
+  var percentTagged = (100 * tagStatTable['TAGGED'] / tp).toStringAsFixed(1);
+  var tagsPerPost = (tagStatTable['TAG_COUNT'] / tp).toStringAsFixed(2);
+  return '${tp} TOTAL POSTS, ${percentTagged}% TAGGED, ${tagsPerPost} TAGS PER POST';
+}
+
 String generateTagReport(td) {
   var sb = new StringBuffer();
   sb.write('<table>');
-  tagFreqTable.forEach((tn, tt) {
+  sb.write('<tr class="rowhead"><td class="celtex">TAG<td class="celnum">#</tr>');
+  var stl = tagFreqTable.keys.toList();
+  stl.sort(byTally);
+  for (var tn in stl) {
+    var tt = tagFreqTable[tn];
     sb.write('<tr><td class="celtex">${tn}<td class="celnum">${tt}</tr>');
-  });
+  }
   sb.write('</table>');
   return sb.toString();
 }
 
 launchTumblrFetch(e) {
+  clearStatTable();
   tagFreqTable.clear();
   tumblrConsole.innerHtml = '';
   tumblrInfo.innerHtml = 'Preparing launch...';
@@ -65,15 +95,20 @@ launchTumblrFetch(e) {
 
 tallyTags(td) {
   for (var p in td['posts']) {
-    tumblrConsole.innerHtml += ("${p['id']} ${p['tags']}<br>");
+    tagStatTable['POST_COUNT'] += 1;
+    //tumblrConsole.innerHtml += ("${p['id']} ${p['tags']}<br>");
     if (p.hasProperty('tags')) {
+      tagStatTable['TAGGED'] += 1;
       for (var t in p['tags']) {
+        tagStatTable['TAG_COUNT'] += 1;
         if (tagFreqTable.containsKey(t)) {
           tagFreqTable[t] += 1;
         } else {
           tagFreqTable[t] = 1;
         }
       }
+    } else {
+      tagStatTable['UNTAGGED'] += 1;
     }
   }
 }
